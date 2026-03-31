62462cfd7cacc245b87bd33ebdcedf72fdbd313c
JuliaWolf208 <Julia2008@yahoo.de>
2026-03-31 18:57:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'wolf-quiz-v1.2'; // Version bei großen Änderungen hochzählen
+const cacheName = 'wolf-quiz-v1.3'; // Version bei großen Änderungen hochzählen
 
 // Beim Installieren cachen wir nur das Nötigste (die App-Hülle)
 self.addEventListener('install', e => {
